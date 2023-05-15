7d546481d2a438b5fe0f845e4306c258736b1586
Weslei Juan Moser Pereira <wesleimsr@gmail.com>
2023-05-15 11:17:10 -0300

    chore: change colorscheme and add some highlights

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,17 +1,30 @@
 vim.opt.termguicolors = true
 vim.o.background = "dark"
 
-require("rose-pine").setup({
-  groups = {},
+require("gruvbox").setup({
+  contrast = "hard",
+  bold = false,
 })
 
-vim.cmd("colorscheme rose-pine")
+vim.cmd("colorscheme gruvbox")
 
 local hl = function(thing, opts)
   vim.api.nvim_set_hl(0, thing, opts)
 end
 
-vim.cmd([[highlight IncludedC guibg=#373b41]])
+vim.cmd([[
+highlight IncludedC guibg=#373b41
+
+highlight DiagnosticUnderlineError cterm=undercurl gui=undercurl
+highlight DiagnosticUnderlineWarn cterm=undercurl gui=undercurl
+highlight DiagnosticUnderlineInfo cterm=undercurl gui=undercurl
+highlight DiagnosticUnderlineHint cterm=undercurl gui=undercurl
+
+highlight link LspReferenceRead DiffChange
+highlight link LspReferenceText DiffChange
+highlight link LspReferenceWrite DiffChange
+highlight link LspSignatureActiveParameter GruvboxOrange
+]])
 
 hl("Bar", {
   cterm = { italic = true, bold = true },
